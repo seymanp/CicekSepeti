7d286ca418da4a30366c490d07d4f2bb5e21717a
Şeyma <lax_files@outlook.com>
2019-05-30 05:03:45 +0300

    Add project files.

---
+++ CicekSepeti/CicekSepeti/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CicekSepeti
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Menu_Clicked(object sender, EventArgs e)
+        {
+            MainMenu menu = new MainMenu();
+            this.Navigation.PushModalAsync(menu);
+
+        }
+
+        private void Cicekler_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
